89770d1672750c7395506aaa31bedc8c55c33f1c
wamah <pythondeveloper21@gmail.com>
2021-02-19 01:06:14 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+import Footer from './Footer'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import SearchPage from './SearchPage'
+
+
+function App() {
+  return (
+    <div className="app">
+     <Router>
+
+     <Header />
+     
+     <Switch>
+     {/* SearchPage */}
+     <Route path="/search">
+     <SearchPage />
+     </Route>
+     <Route path="/">
+     <Home />
+     </Route>
+     
+     </Switch>
+     < Footer />
+     
+     </Router>
+    
+
+     {/* Home */}
+        {/*Header */}
+
+        {/* Banner */}
+            {/* Search */}
+
+        {/*cards */}
+
+        {/* Footer */}
+
+        {/* SearchPage */}
+        {/* Header */}
+        {/*....*/}
+    </div>
+  );
+}
+
+export default App;
